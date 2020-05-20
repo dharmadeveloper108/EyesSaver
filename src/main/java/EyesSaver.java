15b743226767524a242555650c5ab9da36ddbf08
dharmadeveloper108 <whateverland108@gmail.com>
2020-05-20 03:13:21 +0200

    Notification 20 minutes with dialog and notification 2 hours

--- src/main/java/EyesSaver.java
+++ src/main/java/EyesSaver.java
@@ -1,23 +1,12 @@
-import com.intellij.diagnostic.ReportMessages;
-import com.intellij.ide.browsers.BrowserLauncher;
 import com.intellij.notification.*;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.options.newEditor.SettingsDialog;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.ui.DialogBuilder;
 import com.intellij.openapi.ui.DialogWrapper;
-import com.intellij.openapi.ui.popup.Balloon;
-import com.intellij.openapi.ui.popup.JBPopupFactory;
-import com.intellij.openapi.wm.StatusBar;
-import com.intellij.openapi.wm.WindowManager;
-import com.intellij.ui.awt.RelativePoint;
 import org.jetbrains.annotations.NotNull;
-
-import javax.swing.*;
-import javax.swing.event.HyperlinkEvent;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -27,85 +16,86 @@ public class EyesSaver extends AnAction {
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
-        String message = "<html>\n" +
-                " You have looked at the screen for two hours. Time to rest your eyes!" +
-                "  <a href=''" +
+        String message20mins = "<html>\n" +
+                " You have looked at the screen for 20 minutes. <br /> Time to rest your eyes!" +
+                "&nbsp; <a href=''" +
                 ">OK I am ready</a>\n" +
                 "</html>";
 
-        showMyMessage(message);
+        String message2hrs = "<html>\n" +
+                " You have looked at the screen for 2 hours. <br /> Take a 15 minute break!" +
+                "</html>";
+
+        show20minsMessage(message20mins);
+        show2hrsMessage(message2hrs);
 
     }
 
     Timer timer = new Timer();
-    int begin = 0;
-    int timeInterval = 10000;
+    Timer timer1 = new Timer();
+    // every 20 minutes
+    int timeInterval_20min = (1000 * 60) * 20;
+    int begin_20min = timeInterval_20min;
+
+    // every 2 hours
+    int timeInterval_2hrs = ((1000 * 60) * 60) * 2;
+    int begin_2hrs = timeInterval_20min;
 
     public static final NotificationGroup notification_group =
-            new NotificationGroup("My notification group",
+            new NotificationGroup("EyesSaver notification group",
                     NotificationDisplayType.STICKY_BALLOON, true);
 
 
-    void showMyMessage(String message) {
-        ApplicationManager.getApplication().invokeLater(new Runnable() {
+    void show20minsMessage(String message) {
+        ApplicationManager.getApplication().invokeLater(() -> timer.schedule(new TimerTask() {
             @Override
             public void run() {
 
-                timer.schedule(new TimerTask() {
-                    @Override
-                    public void run() {
+                Notification notification = notification_group.createNotification(message, "",
+                        NotificationType.INFORMATION,
+                        (notification1, event) -> {
 
+                            String alert = "Look away from the screen, ideally 20 feet away, for 20 seconds.";
+                            fireDialog_20mins(alert);
 
+                        });
+                Project[] projects = ProjectManager.getInstance().getOpenProjects();
+                notification.setTitle("Eyes Saver");
+                notification.setIcon(PluginIcons.EYEICON);
 
-                        Notification notification = notification_group.createNotification(message, "",
-                                NotificationType.INFORMATION,
-                                new NotificationListener() {
-                                    @Override
-                                    public void hyperlinkUpdate(@NotNull Notification notification, @NotNull HyperlinkEvent event) {
-
-                                        String alert = "Look away from the screen, ideally 20 feet away, for 20 seconds.";
-                                        fireDialog(alert);
-
-                                    }
-                                });
-                        Project[] projects = ProjectManager.getInstance().getOpenProjects();
-                        notification.setTitle("Eyes Saver");
-                        notification.setIcon(PluginIcons.EYEICON);
-
-//                        ShowNotification showNotification = new ShowNotification();
-//                        showNotification.displayTextInToolbar();
-//
-//                        notification.addAction(showNotification);
-                        notification.setContent(message);
-                        Notifications.Bus.notify(notification, projects[0]);
-
-                        //new NotificationDialog().showAndGet();
-
-                    }
-                }, begin, timeInterval);
-
-
+                notification.setContent(message);
+                Notifications.Bus.notify(notification, projects[0]);
 
             }
-        });
+        }, begin_20min, timeInterval_20min));
+
     }
 
+    void show2hrsMessage(String message) {
 
-    void fireDialog(String dialogMessage) {
+        ApplicationManager.getApplication().invokeLater(() -> timer1.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Notification notification = notification_group.createNotification(message,
+                        NotificationType.INFORMATION);
+                Project[] projects = ProjectManager.getInstance().getOpenProjects();
+                notification.setTitle("Eyes Saver");
+                notification.setIcon(PluginIcons.EYEICON);
 
+                notification.setContent(message);
+                Notifications.Bus.notify(notification, projects[0]);
+            }
+        },begin_2hrs, timeInterval_2hrs));
+    }
+
+    void fireDialog_20mins(String dialogMessage) {
         DialogBuilder dialogBuilder = new DialogBuilder();
         dialogBuilder.setDimensionServiceKey("whatever");
         dialogBuilder.setTitle("Eyes Saver");
         dialogBuilder.setErrorText(dialogMessage);
         dialogBuilder.removeAllActions();
-        dialogBuilder.addOkAction();
-        dialogBuilder.addCancelAction();
-
-        boolean isOk = dialogBuilder.show() == DialogWrapper.OK_EXIT_CODE;
-        if(isOk) {
-            dialogBuilder.dispose();
-        }
     }
+
 
 
 }
